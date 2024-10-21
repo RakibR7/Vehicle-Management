844ed4f7282b63e25f8ddce3ec05ad037a224c88
Rakib <G00408644@atu.ie>
2024-10-21 13:39:47 +0100

    V3 fixed vehicle controller and vehicle

--- src/main/java/atu/ie/vehicle_management/Vehicle.java
+++ src/main/java/atu/ie/vehicle_management/Vehicle.java
@@ -5,24 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-@Id
-@GeneratedValue
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vehicle {
     @NotBlank(message = "must not be empty")
     private String vehicleName;
     @NotBlank(message = "must not be empty")
     @Pattern(regexp = "Car.+Truck.+Motorcycle.+Bus")
     private String vehicleType;
-    @Pattern(regexp = "Car.+Truck.+Motorcycle.+Bus")
+    @Pattern(regexp = " .+ ")
     private String registrationNumber;
     @PositiveOrZero
     private float price;
-
     @NotBlank(message = "between 1886 adn current year")
     private int yearOfManufacture;
+    @PositiveOrZero(message = "Cannot be less than 0")
     private int mileage;
-
-
 }
